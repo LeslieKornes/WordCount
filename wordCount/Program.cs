96d968733bb10a2f6d4b4911e8b0de956e00258d
LeslieKornes <lesliekornes@gmail.com>
2017-11-07 19:30:57 -0500

    first and only commit

---
+++ wordCount/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace wordCount
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var filePath = @"C:\Users\lesli\Desktop\paragraph.txt";
+
+            var theString = "";
+
+            using (var fs = new FileStream(filePath, FileMode.Open))
+            using (var sr = new StreamReader(fs))
+            {
+                theString = sr.ReadToEnd();
+            }
+
+            GetWinningWords(theString);
+        }
+
+        private static void GetWinningWords(string theString)
+        {
+            string[] wordArr = theString.Split(new char[] {' ', '.', '"', ',', '-', '!'},
+                StringSplitOptions.RemoveEmptyEntries);
+
+            var winner = wordArr.GroupBy(x => x).Select(z => new
+            {
+                word = z.Key,
+                count = z.Count()
+            }).OrderByDescending(x => x.count);
+
+            foreach (var s in winner)
+            {
+                Console.WriteLine($"{s.word}: {s.count}");
+            }
+        }
+    }
+}
